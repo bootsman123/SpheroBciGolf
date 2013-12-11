16f82c66e13903acc3b74c59ade209bd99dfc17d
Thymen Wabeke <mail@thymen.com>
2013-12-11 13:57:27 +0100

    Removed figures from orginal code + added Java events to imCalibrateStimulus.m

--- SperoBciMatLab/imCalibrateStimulus.m
+++ SperoBciMatLab/imCalibrateStimulus.m
@@ -5,66 +5,77 @@ tgtSeq=mkStimSeqRand(nSymbs,nSeq);
 
 % make the stimulus
 %figure;
-fig=gcf;
-set(fig,'Name','Imagined Movement','color',[0 0 0],'menubar','none','toolbar','none','doublebuffer','on');
-clf;
-ax=axes('position',[0.025 0.025 .95 .95],'units','normalized','visible','off','box','off',...
-        'xtick',[],'xticklabelmode','manual','ytick',[],'yticklabelmode','manual',...
-        'color',[0 0 0],'DrawMode','fast','nextplot','replacechildren',...
-        'xlim',[-1.5 1.5],'ylim',[-1.5 1.5],'Ydir','normal');
-
-stimPos=[]; h=[];
-stimRadius=.5;
-theta=linspace(0,pi,nSymbs); stimPos=[cos(theta);sin(theta)];
-for hi=1:nSymbs; 
-  h(hi)=rectangle('curvature',[1 1],'position',[stimPos(:,hi)-stimRadius/2;stimRadius*[1;1]],...
-                  'facecolor',bgColor); 
-end;
+% fig=gcf;
+% set(fig,'Name','Imagined Movement','color',[0 0 0],'menubar','none','toolbar','none','doublebuffer','on');
+% clf;
+% ax=axes('position',[0.025 0.025 .95 .95],'units','normalized','visible','off','box','off',...
+%         'xtick',[],'xticklabelmode','manual','ytick',[],'yticklabelmode','manual',...
+%         'color',[0 0 0],'DrawMode','fast','nextplot','replacechildren',...
+% %         'xlim',[-1.5 1.5],'ylim',[-1.5 1.5],'Ydir','normal');
+%
+% stimPos=[]; h=[];
+% stimRadius=.5;
+% theta=linspace(0,pi,nSymbs); stimPos=[cos(theta);sin(theta)];
+% for hi=1:nSymbs;
+%   h(hi)=rectangle('curvature',[1 1],'position',[stimPos(:,hi)-stimRadius/2;stimRadius*[1;1]],...
+%                   'facecolor',bgColor);
+% end;
 % add symbol for the center of the screen
-stimPos(:,nSymbs+1)=[0 0];
-h(nSymbs+1)=rectangle('curvature',[1 1],'position',[stimPos(:,nSymbs+1)-stimRadius/4;stimRadius/2*[1;1]],...
-                      'facecolor',bgColor); 
-set(gca,'visible','off');
-
-% play the stimulus
-% reset the cue and fixation point to indicate trial has finished  
-set(h(:),'facecolor',bgColor);
+% stimPos(:,nSymbs+1)=[0 0];
+% h(nSymbs+1)=rectangle('curvature',[1 1],'position',[stimPos(:,nSymbs+1)-stimRadius/4;stimRadius/2*[1;1]],...
+%                       'facecolor',bgColor);
+% set(gca,'visible','off');
+%
+% % play the stimulus
+% % reset the cue and fixation point to indicate trial has finished
+% set(h(:),'facecolor',bgColor);
 sendEvent('stimulus.training','start');
-drawnow; pause(5); % N.B. pause so fig redraws
+sendEvent('DIRECTION_METER_VALUE', pi/2);
+sendEvent('DIRECTION_METER_SHOW');
+
+% drawnow; pause(5); % N.B. pause so fig redraws
 
 for si=1:nSeq;
+    
+    %   if ( ~ishandle(fig) ) break; end;
+    
+    sleepSec(intertrialDuration);
+    %   % show the screen to alert the subject to trial start
+    %   set(h(end),'facecolor',fixColor); % red fixation indicates trial about to start/baseline
+    %   drawnow;% expose; % N.B. needs a full drawnow for some reason
+    sendEvent('BASELINE_SHOW');
+    sendEvent('stimulus.baseline','start');
+    
+    sleepSec(baselineDuration);
+    sendEvent('BASELINE_HIDE');
+    sendEvent('stimulus.baseline','end');
+    
+    
+    % show the target
+    fprintf('%d) tgt=%d : ',si,find(tgtSeq(:,si)>0));
+    %   set(h(tgtSeq(:,si)>0),'facecolor',tgtColor);
+    %   set(h(tgtSeq(:,si)<=0),'facecolor',bgColor);
+    %   set(h(end),'facecolor',[0 1 0]); % green fixation indicates trial running
+    
 
-  if ( ~ishandle(fig) ) break; end;
-
-  sleepSec(intertrialDuration);
-  % show the screen to alert the subject to trial start
-  set(h(end),'facecolor',fixColor); % red fixation indicates trial about to start/baseline
-  drawnow;% expose; % N.B. needs a full drawnow for some reason
-  sendEvent('stimulus.baseline','start');
-  sleepSec(baselineDuration);
-  sendEvent('stimulus.baseline','end');
-  
-  
-  % show the target
-  fprintf('%d) tgt=%d : ',si,find(tgtSeq(:,si)>0));
-  set(h(tgtSeq(:,si)>0),'facecolor',tgtColor);
-  set(h(tgtSeq(:,si)<=0),'facecolor',bgColor);
-  set(h(end),'facecolor',[0 1 0]); % green fixation indicates trial running
-  sendEvent('stimulus.target',find(tgtSeq(:,si)>0));
-  drawnow;% expose; % N.B. needs a full drawnow for some reason
-  sendEvent('stimulus.trial','start');
-  % wait for trial end
-  sleepSec(trialDuration);
-  
-  % reset the cue and fixation point to indicate trial has finished  
-  set(h(:),'facecolor',bgColor);
-  drawnow;
-  sendEvent('stimulus.trial','end');
-  
-  ftime=getwTime();
-  fprintf('\n');
+    sendEvent('stimulus.target',find(tgtSeq(:,si)>0));
+    sendEvent('DIRECTION_METER_VALUE', currentTarget);
+    if(find(tgtSeq(:,si)>0) == 1)
+        sendEvent('DIRECTION_METER_VALUE', currentTarget);
+    else
+        sendEvent('DIRECTION_METER_VALUE', pi);
+    end
+    sendEvent('stimulus.trial','start');
+    sleepSec(trialDuration);
+    
+    sendEvent('DIRECTION_METER_VALUE', pi/2);
+    sendEvent('stimulus.trial','end');
+    
+    ftime=getwTime();
+    fprintf('\n');
 end % sequences
 % end training marker
+sendEvent('DIRECTION_METER_HIDE');
 sendEvent('stimulus.training','end');
 
 % thanks message
--- SperoBciMatLab/imEpochFeedbackStimulus.m
+++ SperoBciMatLab/imEpochFeedbackStimulus.m
@@ -4,53 +4,54 @@ configureIM();
 tgtSeq=mkStimSeqRand(nSymbs,nSeq);
 
 % make the stimulus display
-fig=gcf;
-clf;
-set(fig,'Name','Imagined Movement','color',[0 0 0],'menubar','none','toolbar','none','doublebuffer','on');
-ax=axes('position',[0.025 0.025 .95 .95],'units','normalized','visible','off','box','off',...
-        'xtick',[],'xticklabelmode','manual','ytick',[],'yticklabelmode','manual',...
-        'color',[0 0 0],'DrawMode','fast','nextplot','replacechildren',...
-        'xlim',[-1.5 1.5],'ylim',[-1.5 1.5],'Ydir','normal');
-
-stimPos=[]; h=[];
-stimRadius=.5;
-theta=linspace(0,pi,nSymbs); stimPos=[cos(theta);sin(theta)];
-for hi=1:nSymbs; 
-  h(hi)=rectangle('curvature',[1 1],'position',[stimPos(:,hi)-stimRadius/2;stimRadius*[1;1]],...
-                  'facecolor',bgColor); 
-end;
-% add symbol for the center of the screen
-stimPos(:,nSymbs+1)=[0 0];
-h(nSymbs+1)=rectangle('curvature',[1 1],'position',[stimPos(:,end)-stimRadius/4;stimRadius/2*[1;1]],...
-                      'facecolor',bgColor); 
-set(gca,'visible','off');
-
-
-% play the stimulus
-% reset the cue and fixation point to indicate trial has finished  
-set(h(:),'facecolor',bgColor);
+% fig=gcf;
+% clf;
+% set(fig,'Name','Imagined Movement','color',[0 0 0],'menubar','none','toolbar','none','doublebuffer','on');
+% ax=axes('position',[0.025 0.025 .95 .95],'units','normalized','visible','off','box','off',...
+%         'xtick',[],'xticklabelmode','manual','ytick',[],'yticklabelmode','manual',...
+%         'color',[0 0 0],'DrawMode','fast','nextplot','replacechildren',...
+%         'xlim',[-1.5 1.5],'ylim',[-1.5 1.5],'Ydir','normal');
+% 
+% stimPos=[]; h=[];
+% stimRadius=.5;
+% theta=linspace(0,pi,nSymbs); stimPos=[cos(theta);sin(theta)];
+% for hi=1:nSymbs; 
+%   h(hi)=rectangle('curvature',[1 1],'position',[stimPos(:,hi)-stimRadius/2;stimRadius*[1;1]],...
+%                   'facecolor',bgColor); 
+% end;
+% % add symbol for the center of the screen
+% stimPos(:,nSymbs+1)=[0 0];
+% h(nSymbs+1)=rectangle('curvature',[1 1],'position',[stimPos(:,end)-stimRadius/4;stimRadius/2*[1;1]],...
+%                       'facecolor',bgColor); 
+% set(gca,'visible','off');
+% 
+% 
+% % play the stimulus
+% % reset the cue and fixation point to indicate trial has finished  
+% set(h(:),'facecolor',bgColor);
 sendEvent('stimulus.testing','start');
-drawnow; pause(5); % N.B. pause so fig redraws
+% drawnow; 
+% pause(5); % N.B. pause so fig redraws
 endTesting=false; dvs=[];
 for si=1:nSeq;
 
-  if ( ~ishandle(fig) || endTesting ) break; end;
+%   if ( ~ishandle(fig) || endTesting ) break; end;
   
   sleepSec(intertrialDuration);
   % show the screen to alert the subject to trial start
-  set(h(:),'faceColor',bgColor);
-  set(h(end),'facecolor',fixColor); % red fixation indicates trial about to start/baseline
-  drawnow;% expose; % N.B. needs a full drawnow for some reason
+%   set(h(:),'faceColor',bgColor);
+%   set(h(end),'facecolor',fixColor); % red fixation indicates trial about to start/baseline
+%   drawnow;% expose; % N.B. needs a full drawnow for some reason
   sendEvent('stimulus.baseline','start');
   sleepSec(baselineDuration);
   sendEvent('stimulus.baseline','end');
 
   % show the target
   fprintf('%d) tgt=%d : ',si,find(tgtSeq(:,si)>0));
-  set(h(tgtSeq(:,si)>0),'facecolor',tgtColor);
-  set(h(tgtSeq(:,si)<=0),'facecolor',bgColor);
-  set(h(end),'facecolor',tgtColor); % green fixation indicates trial running
-  drawnow;% expose; % N.B. needs a full drawnow for some reason
+%   set(h(tgtSeq(:,si)>0),'facecolor',tgtColor);
+%   set(h(tgtSeq(:,si)<=0),'facecolor',bgColor);
+%   set(h(end),'facecolor',tgtColor); % green fixation indicates trial running
+%   drawnow;% expose; % N.B. needs a full drawnow for some reason
   sendEvent('stimulus.target',find(tgtSeq(:,si)>0));
   sendEvent('stimulus.trial','start');
   
@@ -92,16 +93,19 @@ for si=1:nSeq;
     fprintf('dv:');fprintf('%5.4f ',pred);fprintf('\t\tProb:');fprintf('%5.4f ',prob);fprintf('\n'); 
   end;  
   [ans,predTgt]=max(dv); % prediction is max classifier output
-  set(h(:),'facecolor',bgColor);
-  set(h(predTgt),'facecolor',tgtColor);
-  drawnow;
+  
   sendEvent('stimulus.predTgt',predTgt);
+  
+%   set(h(:),'facecolor',bgColor);
+%   set(h(predTgt),'facecolor',tgtColor);
+%   drawnow;
+%   sendEvent('stimulus.predTgt',predTgt);
   sleepSec(feedbackDuration);
   
-  % reset the cue and fixation point to indicate trial has finished  
-  set(h(:),'facecolor',bgColor);
-  % also reset the position of the fixation point
-  drawnow;
+%   % reset the cue and fixation point to indicate trial has finished  
+%   set(h(:),'facecolor',bgColor);
+%   % also reset the position of the fixation point
+%   drawnow;
   sendEvent('stimulus.trial','end');
   
   ftime=getwTime();
