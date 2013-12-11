902dc6252696b4dd3ac479f92c71688eb1faf0e4
Thymen Wabeke <mail@thymen.com>
2013-12-11 16:17:36 +0100

    Changed path variables.

--- SperoBciMatLab/runIM.bat
+++ SperoBciMatLab/runIM.bat
@@ -1,2 +1,2 @@
-call ..\utilities\findMatlab.bat
+call "D:\Users\My Documents\MATLAB\buffer_bci\utilities\findMatlab.bat"
 start "matlab" %matexe% -nodesktop -nosplash -minimize -singleCompThread -r "runIM;quit;"
