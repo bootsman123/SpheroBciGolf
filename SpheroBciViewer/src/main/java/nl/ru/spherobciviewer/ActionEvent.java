fa9f0350556361068538516a9c49fed6ee818cfb
Roland Meertens <rolandmeertens@hotmail.com>
2013-12-18 11:03:14 +0100

    Named events to make sure the names are the same as in the java program

--- SpheroBciViewer/src/main/java/nl/ru/spherobciviewer/ActionEvent.java
+++ SpheroBciViewer/src/main/java/nl/ru/spherobciviewer/ActionEvent.java
@@ -12,16 +12,16 @@ public enum ActionEvent
     DIRECTION_METER_SHOW,
     DIRECTION_METER_HIDE,
     DIRECTION_METER_RESET,
-    DIRECTION_METER_VALUE,
+    DIRECTION_METER_VALUE, // 0 up till 2*pi
     DIRECTION_METER_CLOCKWISE,
     DIRECTION_METER_COUNTER_CLOCKWISE,
     POWER_METER_SHOW,
     POWER_METER_RESET,
     POWER_METER_HIDE,
-    POWER_METER_VALUE,
+    POWER_METER_VALUE, // 0 up to 1 
     BASELINE_SHOW,          // Not supported.
     BASELINE_HIDE,          // Not supported.
-    TEXT_SHOW,
+    TEXT_SHOW, // Text frame pops up
     TEXT_HIDE,
-    TEXT_VALUE
+    TEXT_VALUE  // Set the value of the text frame
 };
