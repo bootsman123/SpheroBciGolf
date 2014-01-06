078dc3812ba4c5d3470b475dd5a14c7e1397a042
Bas Bootsma <bootsman@gmail.com>
2014-01-06 10:28:58 +0100

    Updated application.bat

--- SpheroBciMatlab/application.bat
+++ SpheroBciMatlab/application.bat
@@ -1,7 +1,13 @@
-call "C:\Users\bootsman\Documents\MATLAB\Toolboxes\buffer_bci\utilities\findMatlab.bat"
+set path=C:\Users\bootsman\Documents\MATLAB\Toolboxes\buffer_bci
+
+:: Find Matlab path.
+call "%path%\utilities\findMatlab.bat"
 
 :: Start buffer.
-:: ...
+call "%path%\dataAcq\startBuffer.bat"
+
+:: Start signal proxy.
+call "%path%\dataAcq\startSignalProxy.bat"
 
 :: Start application buffer.
 start "matlab" %matexe% -nodesktop -nosplash -minimize -singleCompThread -r "applicationBuffer;quit;"
